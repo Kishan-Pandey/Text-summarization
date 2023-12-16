535c858d5a7753267ece318962bcd3c60c61efa7
Kishan-Pandey <pandeykishan03@gmail.com>
2023-12-16 13:14:44 +0530

    requirements & installation

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ __version__ = "0.0.0"
 
 REPO_NAME = "Text-summarization"
 AUTHOR_USER_NAME = "Kishan-Pandey"
-SRC_REPO = "tectSummarizer"
+SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "pandeykishan03@gmail.com"
 
 setuptools.setup(
