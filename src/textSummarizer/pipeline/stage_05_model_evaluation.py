506851a171ae0ce2b89fc0f73bd5545f44ffc0ac
Kishan-Pandey <pandeykishan03@gmail.com>
2023-12-21 17:09:22 +0530

    model evalation completed

---
+++ src/textSummarizer/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,27 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.model_evaluation import ModelEvaluation
+from textSummarizer.logging import logger
+
+
+
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
